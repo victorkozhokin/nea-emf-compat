b0eb52e76aaef13abb55c0a284c851aa07889077
STRadaT <victorkozhokin@icloud.com>
2026-06-03 23:57:21 +0300

    Initial commit: add project files and Gradle wrapper

---
+++ src/main/java/strm/neaemfcompat/util/PoseSnapshot.java
@@ -0,0 +1,42 @@
+package strm.neaemfcompat.util;
+
+import net.minecraft.client.model.geom.ModelPart;
+
+//как только traben ответит по поводу asm, и бага с отключением анимаций на частях модели, можно будет убрать
+
+public class PoseSnapshot {
+    public final float xRot, yRot, zRot;
+    public final float x, y, z;
+    public final float xScale, yScale, zScale;
+    public final boolean visible;
+    public final boolean skipDraw;
+
+    public PoseSnapshot(ModelPart part) {
+        this.xRot = part.xRot;
+        this.yRot = part.yRot;
+        this.zRot = part.zRot;
+        this.x = part.x;
+        this.y = part.y;
+        this.z = part.z;
+        this.xScale = part.xScale;
+        this.yScale = part.yScale;
+        this.zScale = part.zScale;
+        this.visible = part.visible;
+        this.skipDraw = part.skipDraw;
+    }
+
+    public void apply(ModelPart part) {
+        part.xRot = this.xRot;
+        part.yRot = this.yRot;
+        part.zRot = this.zRot;
+        part.x = this.x;
+        part.y = this.y;
+        part.z = this.z;
+        part.xScale = this.xScale;
+        part.yScale = this.yScale;
+        part.zScale = this.zScale;
+        part.visible = this.visible;
+        part.skipDraw = this.skipDraw;
+    }
+
+}
